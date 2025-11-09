d0743f9a6fe1f34f0a91ea77f325df0067e2b60c
Ten-user <tentlaba13@gmail.com>
2025-11-09 23:12:51 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3kWBiZ2qznhP4VKJfKuOGxHiWsK4jI3Y",
-  authDomain: "career-d232c.firebaseapp.com",
-  projectId: "career-d232c",
-  storageBucket: "career-d232c.appspot.com",
-  messagingSenderId: "488035651405",
-  appId: "1:488035651405:web:14d669c688ae74fa773104",
-  measurementId: "G-VK6M68GG4K"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
