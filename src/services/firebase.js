a7f6b1ae03ddb93b5a1cb53fd9a586aa4c9d30af
Ten-user <tentlaba13@gmail.com>
2025-11-10 06:50:07 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,3 +1,10 @@
+// frontend/src/services/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD3kWBiZ2qznhP4VKJfKuOGxHiWsK4jI3Y",
   authDomain: "career-d232c.firebaseapp.com",
@@ -7,3 +14,11 @@ const firebaseConfig = {
   appId: "1:488035651405:web:14d669c688ae74fa773104",
   measurementId: "G-VK6M68GG4K"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { auth, db, storage, analytics };
