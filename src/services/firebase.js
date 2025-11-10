8ea94c00d65f316253c59810455b5a59c9dfbc8c
Ten-user <tentlaba13@gmail.com>
2025-11-10 06:47:44 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,22 +1,9 @@
-// frontend/src/services/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD3kWBiZ2qznhP4VKJfKuOGxHiWsK4jI3Y",
+  authDomain: "career-d232c.firebaseapp.com",
+  projectId: "career-d232c",
+  storageBucket: "career-d232c.appspot.com",
+  messagingSenderId: "488035651405",
+  appId: "1:488035651405:web:14d669c688ae74fa773104",
+  measurementId: "G-VK6M68GG4K"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
